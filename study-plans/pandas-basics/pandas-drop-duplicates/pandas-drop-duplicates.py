951f87aaa87d4b59df122a905fb47510b3f17817
Ritvik Mod <ritvikmod@gmail.com>
2026-08-21 03:13:00 +0530

    Solve pandas-drop-duplicates

---
+++ study-plans/pandas-basics/pandas-drop-duplicates/pandas-drop-duplicates.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def drop_duplicates(data):
+    """
+    Returns: list [rows_before, rows_after, cleaned_data]
+    """
+    df = pd.DataFrame(data)
+    rows_before = df.shape[0]
+
+    cleaned_data = df.drop_duplicates(keep='first')
+    
+    rows_after = cleaned_data.shape[0]
+
+    return [rows_before,rows_after,cleaned_data.to_dict(orient='list')]
