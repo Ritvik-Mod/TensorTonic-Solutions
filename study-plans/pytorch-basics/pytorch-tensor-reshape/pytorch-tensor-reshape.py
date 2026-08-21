04c00c20ae5638dcf0ef254ac1f82783ce900fc2
Ritvik Mod <ritvikmod@gmail.com>
2026-08-21 22:24:51 +0530

    Solve pytorch-tensor-reshape

---
+++ study-plans/pytorch-basics/pytorch-tensor-reshape/pytorch-tensor-reshape.py
@@ -0,0 +1,16 @@
+import torch
+
+def reshape_tensor(x, op):
+    """
+    Returns: list
+    """
+    x = torch.tensor(x,dtype=torch.float32)
+
+    if op=='flatten':
+        return x.flatten().tolist()
+
+    if op=='squeeze':
+        return x.squeeze().tolist()
+
+    if op=='transpose':
+        return x.T.tolist()
