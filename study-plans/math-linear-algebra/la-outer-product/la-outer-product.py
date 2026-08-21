1f85fdce43e063fcbd621fb3940804cf6dfee6b0
Ritvik Mod <ritvikmod@gmail.com>
2026-08-21 20:20:16 +0530

    Solve la-outer-product

--- study-plans/math-linear-algebra/la-outer-product/la-outer-product.py
+++ study-plans/math-linear-algebra/la-outer-product/la-outer-product.py
@@ -4,6 +4,12 @@ def outer_product(u, v):
     """
     Returns: float64 matrix of shape (m, n), the outer product u v^T.
     """
-    u = np.array(u,dtype=np.float64)[:,None]
-    v = np.array(v, dtype=np.float64)[None,:]
-    return u@v
+    # u = np.array(u,dtype=np.float64)[:,None]
+    # v = np.array(v, dtype=np.float64)[None,:]
+    # return u@v
+
+    u = np.array(u,dtype=np.float64)
+    u = u.reshape(u.shape[0],1)
+    v = np.array(v, dtype=np.float64)
+    v = v.reshape(v.shape[0],1)
+    return u@(v.T)
